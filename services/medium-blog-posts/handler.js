f686b4964972c1d9f55344d6f96cdcce1d2cceec
Justin Fuller <justindanielfuller@gmail.com>
2017-12-28 16:28:21 -0800

    Refactor to use async/await

--- services/medium-blog-posts/handler.js
+++ services/medium-blog-posts/handler.js
@@ -1,40 +1,21 @@
 import 'babel-polyfill';
-import https from 'https';
-import xml from 'xml2js';
+import { get } from 'lodash';
+import { Parser } from 'xml2js';
+import { promisify } from 'bluebird';
+import request from 'request-promise-native';
 
-const parser = new xml.Parser();
+const { parseString } = new Parser();
+const parseStringPromise = promisify(parseString);
 
-const getPostsPromise = () => new Promise(getPosts);
-
-function getPosts(resolve, reject) {
-  https.get('https://medium.com/feed/@justindanielfuller', getData)
-    .on('error', reject);
-
-  function getData (res) {
-    let data = '';
-    res.on('data', addToData);
-    res.on('end', parseData);
-
-    function addToData (d) {
-      data += d;
-    }
-
-    function parseData () {
-      parser.parseString(data, handleParseResult);
-    }
-
-    function handleParseResult (err, result) {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve(result.rss.channel[0].item);
-    }
-  }
+async function getPosts(resolve, reject) {
+  const resultXML = await request('https://medium.com/feed/@justindanielfuller');
+  const resultJSON = await parseStringPromise(resultXML);
+  return get(resultJSON, 'rss.channel[0].item');
 }
 
-export const posts = (event, context, callback) => getPostsPromise()
-  .then(res => {
+export const posts = async (event, context, callback) => {
+  try {
+    const res = await getPosts();
     const response = {
       statusCode: 200,
       headers: {
@@ -43,6 +24,8 @@ export const posts = (event, context, callback) => getPostsPromise()
       },
       body: JSON.stringify(res)
     };
-    callback(null, response);
-  })
-  .catch(callback);
+    return callback(null, response);
+  } catch (e) {
+    return callback(e);
+  }
+};
