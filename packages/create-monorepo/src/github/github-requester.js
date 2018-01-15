85b15e0d7f393afd0e91f390b4592917d90d190b
Justin Fuller <justindanielfuller@gmail.com>
2018-01-15 15:31:01 -0500

    exec as promise.

--- packages/create-monorepo/src/github/github-requester.js
+++ packages/create-monorepo/src/github/github-requester.js
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch';
 import Promise from 'bluebird';
 import { exec } from 'child_process';
+import { promisify } from 'util';
+const execPromise = util.promisify(exec);
 
 const githubRequester = data => ({
   value() {
@@ -13,7 +15,7 @@ const githubRequester = data => ({
     return fetch(data).then(res => res.json()).then(githubRequester);
   },
   downloadRepos(commandFormatter) {
-    return Promise.map(data, repoData => exec(commandFormatter(data))).then(githubRequester);
+    return Promise.map(data, repoData => execPromise(commandFormatter(data))).then(githubRequester);
   },
   formatUserRepoUrl(urlFormatter, USER_REPO_URL_KEY) {
     return githubRequester(urlFormatter(data[USER_REPO_URL_KEY]));
